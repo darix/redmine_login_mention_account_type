064e56d76d4a2a90b0ebebbfa66f3c9c0bb16df4
Marcus Rückert <mrueckert@suse.de>
2014-03-12 16:09:16 +0100

    Hooks should be a class not a module

--- lib/login_mention_account_type/hooks.rb
+++ lib/login_mention_account_type/hooks.rb
@@ -1,7 +1,7 @@
 # vim: set sw=2 sts=2 et tw=80 :
 
 module RedmineLoginMentionAccountType
-  module Hooks < Redmine::Hook::ViewListener
+  class Hooks < Redmine::Hook::ViewListener
     render_on :view_account_login_top,
               :partial => 'view_account_login_top'
   end
